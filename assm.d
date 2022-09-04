9e7fc2d51ad4dc26f39ba92cd2635eca7ffaf5a4
foh-k <fusukekashiwa@gmail.com>
2022-09-05 00:56:02 +0900

    add assembler

---
+++ assm.d
@@ -0,0 +1,159 @@
+/*******************************************************************************
+* AKARIN : Adaptive Kosher Architecture of RIsc processor Next
+*                                   Copyright(C) 2013 Project YY
+*******************************************************************************/
+
+import std;
+
+string assemble(string s)
+{
+    string ret;
+
+    // 命令は一行で記述して次のフォーマット
+    // INST RegD, Reg1, Reg2
+    auto p = s.map!"a.toUpper()".substitute(',', ' ').array.split().array();
+    foreach (ref e; p[1 .. $])
+        e = e[0].isNumber ? e : e[1 .. $];
+
+    enum TYPES
+    {
+        R,
+        I,
+        S,
+        B,
+        U,
+        J
+    }
+
+    TYPES t;
+    // p[0] = inst, p[1] = RegD, p[2] = Reg1, p[3] = Reg2
+    auto rd = format("%05b", p[1].parse!int());
+    // Type-Uが20ビット
+    auto rs1 = format("%020b", parse!int(p[2]));
+    // Type-Iが12ビット
+    auto rs2 = format("%012b", parse!int(p[3]));
+    string opcode, fn3, fn7;
+
+    switch (p[0])
+    {
+    case "ADD":
+        t = TYPES.R;
+        opcode = "0110011";
+        fn3 = "000";
+        fn7 = "0000000";
+        break;
+
+    case "SUB":
+        t = TYPES.R;
+        opcode = "0110011";
+        fn3 = "000";
+        fn7 = "0100000";
+        break;
+
+    case "SLL":
+        t = TYPES.R;
+        opcode = "0110011";
+        fn3 = "001";
+        fn7 = "0000000";
+        break;
+
+    case "SLT":
+        t = TYPES.R;
+        opcode = "0110011";
+        fn3 = "010";
+        fn7 = "0000000";
+        break;
+
+    case "SLTU":
+        t = TYPES.R;
+        opcode = "0110011";
+        fn3 = "011";
+        fn7 = "0000000";
+        break;
+
+    case "XOR":
+        t = TYPES.R;
+        opcode = "0110011";
+        fn3 = "100";
+        fn7 = "0000000";
+        break;
+
+    case "SRL":
+        t = TYPES.R;
+        opcode = "0110011";
+        fn3 = "101";
+        fn7 = "0000000";
+        break;
+
+    case "SRA":
+        t = TYPES.R;
+        opcode = "0110011";
+        fn3 = "101";
+        fn7 = "0100000";
+        break;
+
+    case "OR":
+        t = TYPES.R;
+        opcode = "0110011";
+        fn3 = "110";
+        fn7 = "0000000";
+        break;
+
+    case "AND":
+        t = TYPES.R;
+        opcode = "0110011";
+        fn3 = "111";
+        fn7 = "0000000";
+        break;
+
+    default:
+        // 存在しない命令
+        assert(0);
+    }
+
+    assert(opcode.length == 7);
+    assert(fn3.length == 3);
+    assert(fn7.length == 7);
+
+    switch (t)
+    {
+    case TYPES.R:
+        ret = fn7 ~ rs2[$ - 5 .. $] ~ rs1[$ - 5 .. $] ~ fn3 ~ rd ~ opcode;
+        break;
+    case TYPES.I:
+        // あとで書く
+        break;
+    case TYPES.S:
+        // あとで書く
+        break;
+    case TYPES.B:
+        // あとで書く
+        break;
+    case TYPES.U:
+        // あとで書く
+        break;
+    case TYPES.J:
+        // あとで書く
+        break;
+    default:
+        assert(0);
+    }
+
+    return ret;
+}
+
+// run local
+void main()
+{
+    auto f_r = File("test.asm", "r");
+    auto f_w = File("test.txt", "w");
+
+    string s;
+    while ((s = f_r.readln()) !is null)
+    {
+        // 空行とコメントを読み飛ばし
+        if (s[0] == '#' || s.dup().uniq().array().length <= 2)
+            continue;
+        f_w.writeln(assemble(s));
+    }
+}
---
+++ test.asm
@@ -0,0 +1,3 @@
+add x1, x0, x0
+sub x2, x1, x0
+slt x3, x1, x2
